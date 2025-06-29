8e820f113ab5a3ab44fbfb8ba7b355b18531d5d0
Tigr0K <yufing@mail.ru>
2025-06-29 19:26:30 +0300

    add tests

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -4,7 +4,10 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import java.util.List;
+
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -12,6 +15,12 @@ import static com.codeborne.selenide.Selenide.*;
 public class MainPage {
     private final SelenideElement searchInput = $("#searchInput");
     private final ElementsCollection cardElements = $$(".product-card__wrapper");
+    private final SelenideElement emptyBasketButton = $(".basket-empty__btn");
+    private final ElementsCollection itemNavbar = $$(".navbar-pc__item");
+    private final ElementsCollection listOfTitles = $$(".service-menu__list li");
+    private final SelenideElement searchByImageFormAbNew = $("#searchByImageFormAbNew");
+    private final SelenideElement uploadImageForSearchByImagePopUpContainer = $("#uploadImageForSearchByImagePopUpContainer");
+    private final SelenideElement bannerCarousel = $(".main-page__banner");
 
     @Step("Открываем главную страницу")
     public MainPage openPage() {
@@ -31,4 +40,37 @@ public class MainPage {
         return this;
     }
 
+    @Step("Поиск иконки ${chapter}")
+    public MainPage serachElement(String chapter) {
+        itemNavbar.findBy(text(chapter)).click();
+        return this;
+    }
+    @Step("Появление кнопки при нажатии на пустую страницу")
+    public MainPage buttonEmptyBasket(String buttonText) {
+        emptyBasketButton.shouldHave(text(buttonText));
+        return this;
+    }
+    @Step("Поиск заголовков при нажатии на кнопку")
+    public MainPage titlesExists(List<String> expectedLinks) {
+        listOfTitles.filter(visible).shouldHave(texts(expectedLinks));
+        return this;
+    }
+    @Step("Клик на поиск товара по фото")
+    public MainPage clickPhotoSearch() {
+        searchByImageFormAbNew.shouldBe(visible).click();
+        return this;
+    }
+    @Step("Появление всплывающего меню при клике на поиск по фото")
+    public MainPage popUpPhotoSearch() {
+        uploadImageForSearchByImagePopUpContainer.shouldBe(visible);
+        return this;
+    }
+    @Step("Наличие карусели баннеров")
+    public MainPage  bannerCarouselExist() {
+         bannerCarousel.shouldBe(visible);
+        return this;
+    }
+
 }
+
+
--- src/test/java/tests/WildberriesTests.java
+++ src/test/java/tests/WildberriesTests.java
@@ -1,15 +1,19 @@
 package tests;
 
 import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import pages.MainPage;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import java.util.List;
+import java.util.stream.Stream;
+
 
 
 @Tag("AllTests")
@@ -17,6 +21,7 @@ import static com.codeborne.selenide.Selenide.$$;
 public class WildberriesTests extends TestBase {
 
     MainPage mainPage = new MainPage();
+
     @ValueSource(strings = {"Перфоратор", "Светильник", "Видеокарта"})
     @ParameterizedTest(name = "При поиске запроса {0} список не должен быть пустым")
     @Tag("SMOKE")
@@ -26,4 +31,50 @@ public class WildberriesTests extends TestBase {
                 .checkCardMore(10);
     }
 
+    @CsvSource(value = {"Корзина, Перейти на главную"})
+    @Tag("SMOKE")
+    @ParameterizedTest(name = "Появление кнопки при нажатии на корзину")
+    void buttunForClickOnBasketTest(String chapter, String buttonText) {
+        mainPage.openPage()
+                .serachElement(chapter)
+                .buttonEmptyBasket(buttonText);
+    }
+
+    static Stream<Arguments> titlsOnClickBattonAddress() {
+        return Stream.of(
+                Arguments.of(" Адреса ", List.of(
+                        "Как сделать заказ",
+                        "Способы оплаты",
+                        "Доставка",
+                        "Возврат товара",
+                        "Возврат денежных средств",
+                        "Вопросы и ответы"))
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Заголовки при нажитии на кнопку \"Адреса")
+    void titlsOnClickBattonAddress(String chapter, List<String> expectedLinks) {
+        mainPage.openPage()
+                .serachElement(chapter)
+                .titlesExists(expectedLinks);
+
+    }
+
+    @Tag("SMOKE")
+    @Test
+    @DisplayName("Появление всплывающего меню при клике на поиск по фото")
+    void popUpPhotoSearch(){
+        mainPage.openPage()
+                .clickPhotoSearch()
+                .popUpPhotoSearch();
+    }
+
+    @Tag("SMOKE")
+    @Test
+    @DisplayName("Наличие карусели баннеров")
+    void bannerCarouselExist(){
+        mainPage.openPage()
+                .bannerCarouselExist();
+    }
 }
